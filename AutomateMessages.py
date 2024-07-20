b7afa9c1d9f75574f1237f2cb7a890ad55a9f18c
Justin Le <149338419+Ju5t1nL3@users.noreply.github.com>
2024-07-20 15:22:36 -0500

    Add files via upload

---
+++ AutomateMessages.py
@@ -0,0 +1,18 @@
+import requests
+import time
+import schedule
+from SuperSecret import phone
+
+def task():
+  resp = requests.post('https://textbelt.com/text', {
+    'phone': phone,
+    'message': 'lol.',
+    'key': 'textbelt',
+  })
+  print(resp.json())
+
+schedule.every().day.at('10:00').do(task)
+
+while True:
+  schedule.run_pending()
+  time.sleep(1)
